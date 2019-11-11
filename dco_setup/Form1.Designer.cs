c2f77048bd1875c6dd19e8e3cd24755a5cad9005
arunen <arunen>
2019-11-11 23:27:17 +0530

    Beta version 0.1
    Verify all drivers and JRE_PATH and in case of error show in dialogbox

---
+++ dco_setup/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace dco_setup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnl_main = new System.Windows.Forms.Panel();
+            this.rdbtn_po = new System.Windows.Forms.RadioButton();
+            this.rdbtn_dco = new System.Windows.Forms.RadioButton();
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.btn_install = new System.Windows.Forms.Button();
+            this.btn_verifyDrivers = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkList_drivers = new System.Windows.Forms.CheckedListBox();
+            this.btn_driverPath = new System.Windows.Forms.Button();
+            this.lbl_driverPath = new System.Windows.Forms.Label();
+            this.txt_driverPath = new System.Windows.Forms.TextBox();
+            this.lbl_welcome = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnl_main.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl_main
+            // 
+            this.pnl_main.Controls.Add(this.rdbtn_po);
+            this.pnl_main.Controls.Add(this.rdbtn_dco);
+            this.pnl_main.Controls.Add(this.lbl_status);
+            this.pnl_main.Controls.Add(this.btn_install);
+            this.pnl_main.Controls.Add(this.btn_verifyDrivers);
+            this.pnl_main.Controls.Add(this.groupBox1);
+            this.pnl_main.Controls.Add(this.btn_driverPath);
+            this.pnl_main.Controls.Add(this.lbl_driverPath);
+            this.pnl_main.Controls.Add(this.txt_driverPath);
+            this.pnl_main.Controls.Add(this.lbl_welcome);
+            this.pnl_main.Location = new System.Drawing.Point(12, 12);
+            this.pnl_main.Name = "pnl_main";
+            this.pnl_main.Size = new System.Drawing.Size(631, 327);
+            this.pnl_main.TabIndex = 0;
+            // 
+            // rdbtn_po
+            // 
+            this.rdbtn_po.AutoSize = true;
+            this.rdbtn_po.Location = new System.Drawing.Point(144, 67);
+            this.rdbtn_po.Name = "rdbtn_po";
+            this.rdbtn_po.Size = new System.Drawing.Size(40, 17);
+            this.rdbtn_po.TabIndex = 9;
+            this.rdbtn_po.Text = "PO";
+            this.rdbtn_po.UseVisualStyleBackColor = true;
+            this.rdbtn_po.CheckedChanged += new System.EventHandler(this.rdbtn_po_CheckedChanged);
+            // 
+            // rdbtn_dco
+            // 
+            this.rdbtn_dco.AutoSize = true;
+            this.rdbtn_dco.Location = new System.Drawing.Point(22, 67);
+            this.rdbtn_dco.Name = "rdbtn_dco";
+            this.rdbtn_dco.Size = new System.Drawing.Size(48, 17);
+            this.rdbtn_dco.TabIndex = 8;
+            this.rdbtn_dco.Text = "DCO";
+            this.rdbtn_dco.UseVisualStyleBackColor = true;
+            this.rdbtn_dco.CheckedChanged += new System.EventHandler(this.rdbtn_dco_CheckedChanged);
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.AutoSize = true;
+            this.lbl_status.Location = new System.Drawing.Point(19, 295);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(43, 13);
+            this.lbl_status.TabIndex = 7;
+            this.lbl_status.Text = "Status: ";
+            // 
+            // btn_install
+            // 
+            this.btn_install.Enabled = false;
+            this.btn_install.Location = new System.Drawing.Point(414, 285);
+            this.btn_install.Name = "btn_install";
+            this.btn_install.Size = new System.Drawing.Size(92, 23);
+            this.btn_install.TabIndex = 6;
+            this.btn_install.Text = "Install";
+            this.btn_install.UseVisualStyleBackColor = true;
+            this.btn_install.Click += new System.EventHandler(this.btn_install_Click);
+            // 
+            // btn_verifyDrivers
+            // 
+            this.btn_verifyDrivers.Location = new System.Drawing.Point(316, 285);
+            this.btn_verifyDrivers.Name = "btn_verifyDrivers";
+            this.btn_verifyDrivers.Size = new System.Drawing.Size(92, 23);
+            this.btn_verifyDrivers.TabIndex = 5;
+            this.btn_verifyDrivers.Text = "Verify Drivers";
+            this.btn_verifyDrivers.UseVisualStyleBackColor = true;
+            this.btn_verifyDrivers.Click += new System.EventHandler(this.btn_verifyDrivers_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkList_drivers);
+            this.groupBox1.Location = new System.Drawing.Point(19, 90);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(587, 189);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Drivers List";
+            // 
+            // chkList_drivers
+            // 
+            this.chkList_drivers.Enabled = false;
+            this.chkList_drivers.FormattingEnabled = true;
+            this.chkList_drivers.IntegralHeight = false;
+            this.chkList_drivers.Location = new System.Drawing.Point(25, 28);
+            this.chkList_drivers.Name = "chkList_drivers";
+            this.chkList_drivers.Size = new System.Drawing.Size(537, 154);
+            this.chkList_drivers.TabIndex = 0;
+            // 
+            // btn_driverPath
+            // 
+            this.btn_driverPath.Location = new System.Drawing.Point(439, 39);
+            this.btn_driverPath.Name = "btn_driverPath";
+            this.btn_driverPath.Size = new System.Drawing.Size(92, 23);
+            this.btn_driverPath.TabIndex = 3;
+            this.btn_driverPath.Text = "Select Folder";
+            this.btn_driverPath.UseVisualStyleBackColor = true;
+            this.btn_driverPath.Click += new System.EventHandler(this.btn_driverPath_Click);
+            // 
+            // lbl_driverPath
+            // 
+            this.lbl_driverPath.AutoSize = true;
+            this.lbl_driverPath.Location = new System.Drawing.Point(19, 44);
+            this.lbl_driverPath.Name = "lbl_driverPath";
+            this.lbl_driverPath.Size = new System.Drawing.Size(119, 13);
+            this.lbl_driverPath.TabIndex = 2;
+            this.lbl_driverPath.Text = "Choose Driver Directory";
+            // 
+            // txt_driverPath
+            // 
+            this.txt_driverPath.Location = new System.Drawing.Point(144, 41);
+            this.txt_driverPath.Name = "txt_driverPath";
+            this.txt_driverPath.Size = new System.Drawing.Size(289, 20);
+            this.txt_driverPath.TabIndex = 1;
+            // 
+            // lbl_welcome
+            // 
+            this.lbl_welcome.AutoSize = true;
+            this.lbl_welcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_welcome.Location = new System.Drawing.Point(14, 10);
+            this.lbl_welcome.Name = "lbl_welcome";
+            this.lbl_welcome.Size = new System.Drawing.Size(396, 25);
+            this.lbl_welcome.TabIndex = 0;
+            this.lbl_welcome.Text = "Welcome to DCO & PO Installation Setup";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 350);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Designed by Arunendra Kumar @CDAC-Mumbai";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 372);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnl_main);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DCO & PO Installer Setup v0.1 ";
+            this.pnl_main.ResumeLayout(false);
+            this.pnl_main.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_main;
+        private System.Windows.Forms.Label lbl_welcome;
+        private System.Windows.Forms.Label lbl_driverPath;
+        private System.Windows.Forms.TextBox txt_driverPath;
+        private System.Windows.Forms.Button btn_driverPath;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckedListBox chkList_drivers;
+        private System.Windows.Forms.Button btn_verifyDrivers;
+        private System.Windows.Forms.Button btn_install;
+        private System.Windows.Forms.Label lbl_status;
+        private System.Windows.Forms.RadioButton rdbtn_po;
+        private System.Windows.Forms.RadioButton rdbtn_dco;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ dco_setup/Form1.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace dco_setup
+{
+    public partial class Form1 : Form
+    {
+        private int checkedCount = 0;
+        private string[] driversList = new string[11];
+        private string driverFor = "";
+        public Form1()
+        {
+            InitializeComponent();
+            driverFor = "DCO";
+            loadDCODriversList();
+            txt_driverPath.Text = @"E:\BSID_1\sid drivers";
+            rdbtn_dco.Checked = true;
+        }
+
+        private void loadDCODriversList()
+        {           
+            chkList_drivers.Items.Add("Camera Canon");
+            chkList_drivers.Items.Add("Fortis Client");
+            chkList_drivers.Items.Add("OpenCV 2.4.11");
+            chkList_drivers.Items.Add("Scanner Epson");
+            chkList_drivers.Items.Add("Signature Pad");
+            chkList_drivers.Items.Add("Anaconda3");
+            chkList_drivers.Items.Add("Test Setup");
+        }
+
+        private void loadPODriversList()
+        {
+            chkList_drivers.Items.Add("Fortis Client");
+            chkList_drivers.Items.Add("Printer");
+            chkList_drivers.Items.Add("Java JDK");
+            chkList_drivers.Items.Add("Token");
+            chkList_drivers.Items.Add("PKCS Config");
+            chkList_drivers.Items.Add("gpj");
+            chkList_drivers.Items.Add("Encode Printer");
+        }
+
+        private void btn_driverPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog browserDialog = new FolderBrowserDialog();
+            if (browserDialog.ShowDialog() == DialogResult.OK)
+            {
+                txt_driverPath.Text = browserDialog.SelectedPath;
+            }
+        }
+
+        private void btn_verifyDrivers_Click(object sender, EventArgs e)
+        {
+            checkedCount = 0;
+            string[] directories;
+            try
+            {
+                directories = Directory.GetDirectories(txt_driverPath.Text);
+            }catch(Exception ex)
+            {
+                DialogResult dr = MessageBox.Show("Verify Drivers: ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (driverFor.Equals("DCO"))
+            {
+                foreach (string directory in directories)
+                {
+                    //Console.WriteLine(directory);
+                    setDCOCheckedToDriver(directory);
+                }
+                if (checkedCount == 7)
+                {
+                    btn_install.Enabled = true;
+                    chkList_drivers.Enabled = true;
+                }
+            }
+            else if (driverFor.Equals("PO"))
+            {
+                foreach (string directory in directories)
+                {
+                    //Console.WriteLine(directory);
+                    setPOCheckedToDriver(directory);
+                }
+                //Console.WriteLine(checkedCount);
+                if (checkedCount == 7)
+                {
+                    btn_install.Enabled = true;
+                    chkList_drivers.Enabled = true;
+                }
+            }
+        }
+
+        private void setDCOCheckedToDriver(string directory)
+        {
+            string dir=directory.Substring(directory.LastIndexOf('\\')+1).ToLower();
+            switch (dir)
+            {
+                case "canon eos 1200 sdk":
+                    if (File.Exists(directory + @"\setup.exe"))
+                    {
+                        chkList_drivers.SetItemChecked(0, true);
+                        driversList[0] = directory + @"\setup.exe";
+                        checkedCount++;
+                    }
+                    break;
+                case "fortis client":
+                    if (File.Exists(directory + @"\FortiClientOnlineInstaller.exe"))
+                    {
+                        chkList_drivers.SetItemChecked(2, true);
+                        driversList[1] = directory + @"\FortiClientOnlineInstaller.exe";
+                        checkedCount++;
+                    }
+                    break;
+                case "opencv":
+                    if (File.Exists(directory + @"\opencv-2.4.11.exe"))
+                    {
+                        chkList_drivers.SetItemChecked(3, true);
+                        driversList[2] = directory + @"\opencv-2.4.11.exe";
+                        checkedCount++;
+                    }
+                    break;
+                case "scanner driver":
+                    if (File.Exists(directory + @"\setup_full_5590_20_0_0_002.exe"))
+                    {
+                        chkList_drivers.SetItemChecked(4, true);
+                        driversList[3] = directory + @"\setup_full_5590_20_0_0_002.exe";
+                        checkedCount++;
+                    }
+                    break;
+                case "signature pad":
+                    if (File.Exists(directory + @"\sigplusnet.exe"))
+                    {
+                        chkList_drivers.SetItemChecked(5, true);
+                        driversList[4] = directory + @"\sigplusnet.exe";
+                        checkedCount++;
+                    }
+                    break;
+                case "anaconda3":
+                    if (File.Exists(directory + @"\Anaconda3-4.3.0.1-Windows-x86_64.exe"))
+                    {
+                        chkList_drivers.SetItemChecked(1, true);
+                        driversList[5] = directory + @"\Anaconda3-4.3.0.1-Windows-x86_64.exe";
+                        checkedCount++;
+                    }
+                    break;
+                case "test1":
+                    if (File.Exists(directory + @"\setup.exe"))
+                    {
+                        chkList_drivers.SetItemChecked(6, true);
+                        driversList[6] = directory + @"\setup.exe";
+                        checkedCount++;
+                    }
+                    break;
+            }
+        }
+
+        private void setPOCheckedToDriver(string directory)
+        {
+            string dir = directory.Substring(directory.LastIndexOf('\\') + 1).ToLower();
+            //Console.WriteLine("switch: " + dir);
+            switch (dir)
+            {
+                case "fortis client":
+                    if (File.Exists(directory + @"\FortiClientOnlineInstaller.exe"))
+                    {
+                        chkList_drivers.SetItemChecked(0, true);
+                        driversList[0] = directory + @"\FortiClientOnlineInstaller.exe";
+                        checkedCount++;
+                    }
+                    break;
+                case "printer":
+                    int j = 1;
+                    int count = 0;
+                    string[] printerDrivers = new string[] {"510994_RevF_FargoSDK_v2.1.0.5.exe","511191W_RevF_HDP5000_v2.7.0.3.4_Setup (1).exe","hdp5000_drv_2305.exe","HID_OMNIKEY5x2x_x64_W8_R1_2_26_140.exe" };
+                    foreach(string printerDriver in printerDrivers)
+                    {
+                        if (File.Exists(directory +@"\"+printerDriver))
+                        {
+                            driversList[j++] = directory + @"\" + printerDriver;
+                            count++;
+                        }
+                    }
+                    if (count == printerDrivers.Length)
+                    {
+                        chkList_drivers.SetItemChecked(1, true);
+                        checkedCount++;
+                    }                    
+                    break;
+                case "jdk":
+                    if (File.Exists(directory + @"\jdk-8u151-windows-x64.exe"))
+                    {
+                        chkList_drivers.SetItemChecked(2, true);
+                        driversList[5] = directory + @"\jdk-8u151-windows-x64.exe";
+                        checkedCount++;
+                    }
+                    break;
+                case "token":
+                    j = 6;
+                    count = 0;
+                    string[] tokenDrivers = new string[] { "eToken_PKIClient-x64bit-5.1SP1-Beta.msi", "SafeNetAuthenticationClient-x64-8.3.msi" };
+                    foreach(string tokenDriver in tokenDrivers)
+                    {
+                        if (File.Exists(directory + @"\" + tokenDriver))
+                        {
+                            driversList[j++] = directory + @"\" + tokenDriver;
+                            count++;
+                        }
+                    }
+                    if (count == 2)
+                    {
+                        chkList_drivers.SetItemChecked(3, true);
+                        checkedCount++;
+                    }
+                    break;
+                case "pkcs config":
+                    if (File.Exists(directory + @"\pkcs11.cfg"))
+                    {
+                        chkList_drivers.SetItemChecked(4, true);
+                        driversList[8] = directory + @"\pkcs11.cfg";
+                        checkedCount++;
+                    }
+                    break;
+                case "gpj":
+                    if (File.Exists(directory + @"\AppletRun.bat"))
+                    {
+                        chkList_drivers.SetItemChecked(5, true);
+                        driversList[9] = directory + @"\AppletRun.bat";
+                        checkedCount++;
+                    }
+                    break;
+                case "encode printer":
+                    if (File.Exists(directory + @"\EncodePrinter.exe"))
+                    {
+                        chkList_drivers.SetItemChecked(6, true);
+                        driversList[10] = directory + @"\EncodePrinter.exe";
+                        checkedCount++;
+                    }
+                    break;
+            }
+        }
+
+        private void btn_install_Click(object sender, EventArgs e)
+        {
+            Process p;
+            int installedCounter = 0;
+            List<int> list = new List<int>();
+
+            chkList_drivers.Enabled = false;
+            foreach (int itemChecked in chkList_drivers.CheckedIndices)
+            {
+                list.Add(itemChecked);
+            }
+
+            for (int i=0; i<driversList.Length;i++)
+            {
+                try
+                {
+                    if (driverFor.Equals("PO") && (i==8||i==9))
+                    {
+                        //Console.WriteLine(driversList[i]);
+                        continue;
+                    }
+                    if (driverFor.Equals("DCO"))
+                    {
+                        if (!list.Contains(i))
+                        {
+                            continue;
+                        }
+                    }
+                    //Console.WriteLine(driversList[i]);
+                    
+                    p = new Process();
+                    p.StartInfo.FileName = driversList[i];
+                    p.Start();
+                    while (true)
+                    {
+                        if (p.HasExited == true)
+                        {
+                            break;
+                        }
+                    }
+                    p.WaitForExit();
+                    
+                }catch(Exception ex)
+                {
+                    DialogResult dr = MessageBox.Show("Install: "+ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                installedCounter++;
+            }
+
+            if (driverFor.Equals("PO"))
+            {
+                //copy pkcs11.cfg
+                string jrePath = Environment.GetEnvironmentVariable("JRE_HOME");
+                if (string.IsNullOrEmpty(jrePath))
+                {
+                    DialogResult dr = MessageBox.Show("Please set JRE_HOME environment variable", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                string pkcsPath = jrePath+@"lib\security\pkcs11.cfg";
+                File.Copy(driversList[8], pkcsPath, true);
+                string javaSecurity= jrePath+ @"lib\security\java.security";
+                string key = "security.provider.11=sun.security.pkcs11.SunPKCS11 "+jrePath+@"lib\security";
+                if (!File.Exists(javaSecurity))
+                {
+                    DialogResult dr = MessageBox.Show("java.security file doesn't exist", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                try
+                {
+                    File.AppendAllText(javaSecurity, key);
+                }catch(Exception ex)
+                {
+                    DialogResult dr = MessageBox.Show("Unable to write security path in java.security", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                installedCounter++;
+
+                //copy gpj files
+                try
+                {
+                    string desktopLocation = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\gpj\";
+                    string gpjFilesPath = driversList[9].Substring(0, driversList[9].LastIndexOf('\\'));
+                    if (!Directory.Exists(desktopLocation))
+                    {
+                        Directory.CreateDirectory(desktopLocation);
+                    }
+
+                    DirectoryInfo source = new DirectoryInfo(gpjFilesPath);
+                    foreach (FileInfo file in source.GetFiles())
+                    {
+                        file.CopyTo(desktopLocation + file, true);
+                    }
+                    installedCounter++;
+                }catch(Exception ex)
+                {
+                    //Console.WriteLine(ex.Message);
+                    DialogResult dr = MessageBox.Show("Copy gpj: "+ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            Console.WriteLine("count:"+installedCounter);
+            if (driverFor.Equals("DCO") && installedCounter == 7)
+            {
+                lbl_status.Text = "Installation completed successfully";
+                lbl_status.ForeColor = Color.ForestGreen;
+            }
+            else if (driverFor.Equals("PO") && installedCounter == 11) {
+                lbl_status.Text = "Installation completed successfully";
+                lbl_status.ForeColor = Color.ForestGreen;
+            }
+            else
+            {
+                lbl_status.Text = "Error: All drivers are not installed";
+                lbl_status.ForeColor = Color.DarkRed;
+            }
+        }
+
+        private void rdbtn_po_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbtn_po.Checked)
+            {
+                driverFor = rdbtn_po.Text;
+                chkList_drivers.Items.Clear();
+                loadPODriversList();
+            }
+        }
+
+        private void rdbtn_dco_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbtn_dco.Checked)
+            {
+                driverFor = rdbtn_dco.Text;
+                chkList_drivers.Items.Clear();
+                loadDCODriversList();
+            }
+        }
+    }
+}
